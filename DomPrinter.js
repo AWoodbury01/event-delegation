81bd4292389d564f39d436165c0719c29cbea96a
Ashon Woodbury <a.woodbury01@gmail.com>
2020-08-14 11:03:44 -0400

    pushing to gh

---
+++ DomPrinter.js
@@ -0,0 +1,15 @@
+function buildTaskForm(){
+    return `
+    <form>
+        <input id="new-task-input" type="text" placeholder="New Task"
+    </form>
+    <button id="new-task-submit-btn">Submit</button>
+    `
+}
+
+function buildSingleTask(nameOfTask){
+    return `<section>
+        <p>${Date.now().toString()}</p>
+        <p>${nameOfTask}</p>
+    </section>`
+}
--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-function buildTaskForm(){
-    return `
-    <form>
-        <input type="text" placeholder="New Task"
-    </form>
-    <button id="new-task-submit-btn">Submit</button>
-    `
-}
-
-//Function that builds and printes a single task
-document.querySelector("#my-to-do-list").addEventListener("click", function(){
+// Function that builds and prints a single task
+document.querySelector("#pagecontent").addEventListener("click", function(){
     if (event.target.id === "new-task-submit-btn") {
         console.log("This is the submit button")
+        const taskValue = document.querySelector("#new-task-input").value
+        document.querySelector("#pagecontent").innerHTML += buildSingleTask(taskValue)
     }
 })
 
+// Navbar buttons
 document.querySelector("#my-to-do-list").addEventListener("click",
 function(){
     const pageContentContainer = document.querySelector("#pagecontent")
